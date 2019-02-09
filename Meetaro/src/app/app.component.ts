cd77f42f4dd37ee46b119df1c18bda3b5bccee23
ZenBook/Ineskay <victordivi98@gmail.com>
2019-02-09 10:05:28 +0100

    Create main folder

--- Meetaro/src/app/app.component.ts
+++ Meetaro/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Meetaro';
 
+  constructor(private router: Router) {
+  }
+
   logIn() {
-    
+    this.router.navigate([`/main`]);
   }
 }
